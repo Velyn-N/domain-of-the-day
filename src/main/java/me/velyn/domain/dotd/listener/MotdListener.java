9c921b991f39d0b6b46c5eb1fb34ec7bb4c90d5c
Velyn-N <nils01.gaming@gmail.com>
2026-04-20 22:31:53 +0200

    Handle all MOTD Events again (sometimes Paper Events dont seem to work?)

--- src/main/java/me/velyn/domain/dotd/listener/MotdListener.java
+++ src/main/java/me/velyn/domain/dotd/listener/MotdListener.java
@@ -7,7 +7,9 @@ import me.velyn.domain.dotd.actions.ServerIconAction;
 import me.velyn.domain.dotd.actions.ServerListPlayersAction;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.server.ServerListPingEvent;
 
+import java.net.InetSocketAddress;
 import java.util.List;
 import java.util.UUID;
 import java.util.logging.Logger;
@@ -23,9 +25,52 @@ public class MotdListener implements Listener {
     }
 
     @EventHandler
-    public void onMOTDRequest(PaperServerListPingEvent event) {
-        String hostName = event.getHostname();
+    public void onLegacyMOTDRequest(ServerListPingEvent event) {
+        String hostName = null;
+        PaperServerListPingEvent paperEvent = null;
+        if ("com.destroystokyo.paper.network.StandardPaperServerListPingEventImpl"
+                .equals(event.getClass().getCanonicalName())) {
+            try {
+                Object status = event.getClass().getMethod("getClient").invoke(event);
+                InetSocketAddress virtualHost = (InetSocketAddress) status.getClass().getMethod("getVirtualHost")
+                        .invoke(status);
+                hostName = virtualHost.getHostString();
+            } catch (Exception e) {
+                if (configManager.isDebugLog()) {
+                    log.warning(String.format("Error while using Paper Workaround: %s", e));
+                }
+            }
+            try {
+                paperEvent = (PaperServerListPingEvent) event;
+            } catch (Exception e) {
+                if (configManager.isDebugLog()) {
+                    log.warning(String.format("Event is not a PaperServerListPingEvent: %s", e));
+                }
+            }
+        }
+        if (hostName == null) {
+            if (configManager.isDebugLog()) {
+                log.warning("Paper Workaround for reading MOTD Ping Hostnames did not work, falling back to API Method...");
+            }
+            hostName = event.getHostname();
+        }
 
+        defaultHandling(event, hostName);
+
+        final PaperServerListPingEvent finPaperEvent = paperEvent;
+        configManager.getDomainAction(hostName, ServerListPlayersAction.class)
+                .ifPresent(slpAction -> {
+                    if (finPaperEvent == null) {
+                        if (configManager.isDebugLog()) {
+                            log.info(String.format("Not a Paper Event, cannot handle ServerListPlayersAction: %s", slpAction.getClass().getName()));
+                        }
+                    } else {
+                        handlePlayerAction(slpAction, finPaperEvent);
+                    }
+                });
+    }
+
+    private void defaultHandling(ServerListPingEvent event, String hostName) {
         if (configManager.isDebugLog()) {
             log.info(String.format("Received MOTD Ping for Domain '%s'", hostName));
         }
@@ -33,6 +78,13 @@ public class MotdListener implements Listener {
                      .ifPresent(motdAction -> event.motd(motdAction.getMotd()));
         configManager.getDomainAction(hostName, ServerIconAction.class)
                 .ifPresent(iconAction -> event.setServerIcon(iconAction.getIcon()));
+    }
+
+    @EventHandler
+    public void handlePaper(PaperServerListPingEvent event) {
+        String hostName = event.getHostname();
+
+        defaultHandling(event, hostName);
         configManager.getDomainAction(hostName, ServerListPlayersAction.class)
                 .ifPresent(slpAction -> handlePlayerAction(slpAction, event));
     }
